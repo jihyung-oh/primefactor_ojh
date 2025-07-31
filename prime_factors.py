496e0fe27d473ba88aa188a76fde2e2b66d05d2d
jihyung <lifehappy@naver.com>
2025-07-31 12:50:56 +0900

    [refactor] p53-2

--- prime_factors.py
+++ prime_factors.py
@@ -7,5 +7,5 @@ class PrimeFactor:
             while number % divisor == 0:
                 factors.append(divisor)
                 number //= divisor
-                divisor += 1
+            divisor += 1
         return factors
